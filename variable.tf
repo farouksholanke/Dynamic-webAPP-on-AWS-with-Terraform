12324161f57b623ec6dd1c991d818d15e8c9dc6b
Farouk Sholanke <zayd.farouk@gmail.com>
2023-11-13 18:11:31 -0500

    created rds file

--- variable.tf
+++ variable.tf
@@ -47,3 +47,28 @@ variable "ssh_location" {
  description = "the ip address that can ssh into the ec2 instances"
  type        = string
 }
+
+#rds variables
+variable "database_snapshot_identifier" {
+ default     = "arn:aws:rds:us-east-1:065759301263:snapshot:fleetcart-final-snapshot"
+ description = "database snapshot arn"
+ type        = string
+}
+
+variable "database_instance_class" {
+ default     = "db.t2.micro"
+ description = "the database instance type"
+ type        = string
+}
+
+variable "database_instance_identifier" {
+ default     = "dev-rds-db"
+ description = "the database instance identifier"
+ type        = string
+}
+
+variable "multi_az_deployment" {
+ default     = false
+ description = "create a standby db instance"
+ type        = bool
+}
